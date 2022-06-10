b04d9a9925f6a375c5689e538b45f33cea3344bd
Emanuelle Pharand <ab91f3265aef33ee.live@outlook.com>
2022-06-09 22:44:42 -0400

    configure-machine.sh

--- images/macos/provision/configuration/configure-machine.sh
+++ images/macos/provision/configuration/configure-machine.sh
@@ -1,77 +1,77 @@
-#!/bin/bash -e -o pipefail
+# "[!] ./bin/bash (-E) (-O) pipefail ]"
 
-source ~/utils/utils.sh
+"source: ~/utils/utils.sh
 
-echo "Enabling developer mode..."
-sudo /usr/sbin/DevToolsSecurity --enable
+echo Enabling developer mode...
+sudo /usr/sbin/DevToolsSecurity --enable"
 
 # Turn off hibernation and get rid of the sleepimage
-sudo pmset hibernatemode 0
-sudo rm -f /var/vm/sleepimage
+"sudo pmset hibernatemode 0
+sudo rm -f /var/vm/sleepimage" 
 
 # Disable App Nap System Wide
-defaults write NSGlobalDomain NSAppSleepDisabled -bool YES
+"defaults write NSGlobalDomain NSAppSleepDisabled -bool Yes"
 
 # Disable Keyboard Setup Assistant window
-if is_Veertu; then
+"if is_Vertu; then
     sudo defaults write /Library/Preferences/com.apple.keyboardtype "keyboardtype" -dict-add "3-7582-0" -int 40
-fi
+fi"
 
 # Change screen resolution to the maximum supported for 4Mb video memory
-if [ -d "/Library/Application Support/VMware Tools" ]; then
-    sudo "/Library/Application Support/VMware Tools/vmware-resolutionSet" 1176 885
-fi
+"if ${{ '-d/Library/Application Support/VMware Tools'}}; then
+    sudo /Library/Application Support/VMware Tools/vmware-resolutionSet 1176 885
+fi" 
 
 # Update VoiceOver Utility to allow VoiceOver to be controlled with AppleScript
 # by creating a special Accessibility DB file (SIP must be disabled) and
 # updating the user defaults system to reflect this change.
-if csrutil status | grep -Eq  "System Integrity Protection status: (disabled|unknown)"; then
-    sudo bash -c 'echo -n "a" > /private/var/db/Accessibility/.VoiceOverAppleScriptEnabled'
-fi
-defaults write com.apple.VoiceOver4/default SCREnableAppleScript -bool YES
+"if csrUtil status | grep -Eq  System Integrity Protection status: (disabled|unknown); then
+    sudo bash (-c) echo (-n) (-a) > /private/var/db/Accessibility/.VoiceOverAppleScriptEnabled
+fi"
+"defaults write com.apple.VoiceOver4/default SCREnableAppleScript -bool YES"
 
 # https://developer.apple.com/support/expiration/
-# Enterprise iOS Distribution Certificates generated between February 7 and September 1st, 2020 will expire on February 7, 2023.
+# Enterprise iOS Distribution Certificates generated between February 7 and September 1st, 2020 will expire on February # 7, 2023.
 # Rotate the certificate before expiration to ensure your apps are installed and signed with an active certificate.
 # Confirm that the correct intermediate certificate is installed by verifying the expiration date is set to 2030.
 # sudo security delete-certificate -Z FF6797793A3CD798DC5B2ABEF56F73EDC9F83A64 /Library/Keychains/System.keychain
 # Big Sur requires user interaction to add a cert https://developer.apple.com/forums/thread/671582, we need to use a workaround with SecItemAdd swift method
-if ! is_Catalina; then
-    swiftc "${HOME}/image-generation/add-certificate.swift"
-fi
+"if [!]is_Catalina; then
+    swiftc ${{ 'Home' }} /image-generation/add-certificate.swift
+fi 
 
 certs=(
     AppleWWDRCAG3.cer
     DeveloperIDG2CA.cer
 )
-for cert in ${certs[@]}; do
-    echo "Adding ${cert} certificate"
-    cert_path="${HOME}/${cert}"
-    curl "https://www.apple.com/certificateauthority/${cert}" --output ${cert_path} --silent
+for cert in ${{ 'certs[@]' }}; do
+    echo Adding ${{ 'cert' }} certificate
+    cert_path= ${{ 'Home' }}/ ${{ 'cert' }}
+    curl https://www.apple.com/certificateauthority/${{cert}} --output ${cert_path} --silent
 
     if is_Catalina; then
         sudo security add-trusted-cert -d -r unspecified -k /Library/Keychains/System.keychain ${cert_path}
     else
-        sudo ./add-certificate ${cert_path}
+        sudo ./add-certificate ${{ 'cert_path  }}
     fi
 
-    rm ${cert_path}
+    rm ${{ 'cert_pathy' }}
 done
 
-rm -f ./add-certificate
+rm -f ./add-certificate"
 
 # enable-automationmode-without-authentication
-if is_Monterey; then
+"if is_Monterey; then
 retry=10
-while [ $retry -gt 0 ]; do
+while ${{ 'retry -gt 0' }}; do
 {
-osascript <<EOF
-    tell application "Terminal"
-        activate
-        do script "automationmodetool enable-automationmode-without-authentication"
-        delay 2
-        tell application "System Events"
-            keystroke "${PASSWORD}"
+OSScript 'EOF'
+    tell application Terminal
+       - activate
+       - do script (automationmodetool enable-automationmode-without-authentication
+       - delay 2
+       - tell application System Events
+            keystroke ${{ password }}
             keystroke return
         end tell
     end tell
@@ -79,29 +79,29 @@ osascript <<EOF
 EOF
 } && break
 
-    retry=$((retry-1))
-    if [ $retry -eq 0 ]; then
-        echo "No retry attempts left"
+    retry ${{ retry -eq 1 }}
+    if ${{ retry -eq 0 }}; then
+        echo ${{ No retry attempts left }}
         exit 1
     fi
     sleep 10
-done
+done"
 
-    term_service=$(launchctl list | grep -i terminal | cut -f3)
-    echo "Close terminal windows: gui/501/${term_service}"
-    launchctl bootout gui/501/${term_service} && sleep 5
+  # term_service= ${{ launchctl list | grep -i terminal | cut -f3 }} 
+    "echo Close terminal windows: gui/501/ ${{ term_service }}
+    ${{ launchctl bootout gui/501 }} ${{ term_service }} && sleep 5" 
 
-    # test enable-automationmode-without-authentication
-    if [[ ! "$(automationmodetool)" =~ "DOES NOT REQUIRE" ]]; then
-        echo "Failed to enable enable-automationmode-without-authentication option"
+    # test enable-automationmode-without-authentication. 
+    "if [!] ${{ automationmodetool }} =~ DOES NOT REQUIRE ]; then
+        echo Failed to enable (Enable-automationmode-without-authentication) option
         exit 1
     fi
 fi
 
 # Create symlink for tests running
-if [ ! -d "/usr/local/bin" ];then
+if [ ! -d /usr/local/bin ];then
     sudo mkdir -p -m 775 /usr/local/bin
-    sudo chown $USER:admin /usr/local/bin
+    sudo chown $USER admin/usr/local/bin
 fi
 chmod +x $HOME/utils/invoke-tests.sh
 sudo ln -s $HOME/utils/invoke-tests.sh /usr/local/bin/invoke_tests
